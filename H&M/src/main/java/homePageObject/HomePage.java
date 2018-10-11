34266018569764746b2710123340acb94ce69368
shamsetubriz <qjshams@gmail.com>
2018-10-11 02:12:56 -0400

    New Module H&M added

--- H&M/src/main/java/homePageObject/HomePage.java
+++ H&M/src/main/java/homePageObject/HomePage.java
@@ -1,6 +1,7 @@
 package homePageObject;
 
 import base.BaseUtil;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import reporting.TestLogger;
@@ -27,22 +28,48 @@ public class HomePage extends BaseUtil {
     @FindBy(xpath = "//a[@href='/en_us/sale.html']")
     public static WebElement saleLink;
 
+    @FindBy(xpath = "//*[@id=\'main-search\']")
+    public static WebElement searchbox;
+
     public void sendValueToEmailBox(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         email.sendKeys("value");
     }
 
-
     public void signInToHnM(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         signInLink.click();
         email.sendKeys("ahmedFagol");
         password.sendKeys("fahimFanku");
         signInButton.click();
     }
+
     public void signInByDataProvider(String userEmail, String passCode) throws InterruptedException{
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         email.sendKeys(userEmail);
         password.sendKeys(passCode);
         signInButton.click();
         Thread.sleep(3000);
+    }
+
+    public void navigateToMensPage(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        menLink.click();
+    }
+
+    public void navigateToWomenPage(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        womenLink.click();
+    }
+
+    public void checkOutSaleLink(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        saleLink.isEnabled();
+    }
+
+    public void searchItem(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        searchbox.click();
+        searchbox.sendKeys("laal Jainga for boro hujur", Keys.ENTER);
     }
 }
--- H&M/src/test/java/homePageObjectTest/HomePageTest.java
+++ H&M/src/test/java/homePageObjectTest/HomePageTest.java
@@ -4,6 +4,7 @@ import homePageObject.HomePage;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import reporting.TestLogger;
 
 public class HomePageTest extends HomePage {
 
@@ -15,15 +16,39 @@ public class HomePageTest extends HomePage {
 
     @Test
     public void testSignInToHnM(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         objHomePage.signInToHnM();
     }
+
     @Test
     public void testSignInLink(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         signInLink.isEnabled();
     }
     @Test
     public void testSendValueToEmailBox(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         signInLink.click();
         objHomePage.sendValueToEmailBox();
+    }
+    @Test
+    public void testNavigateToMensPage(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        objHomePage.navigateToMensPage();
+    }
+    @Test
+    public void testNavigateToWomenPage(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        objHomePage.navigateToWomenPage();
+    }
+    @Test
+    public void testCheckOutSaleLink(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        objHomePage.checkOutSaleLink();
+    }
+    @Test
+    public void testSearchItem(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        objHomePage.searchItem();
     }
 }
